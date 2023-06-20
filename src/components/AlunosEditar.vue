f412cc8c23ac1d340953420583334a6b09278d34
programalene <133455186+programalene@users.noreply.github.com>
2023-06-19 21:38:31 -0300

    Update AlunosEditar.vue

--- src/components/AlunosEditar.vue
+++ src/components/AlunosEditar.vue
@@ -35,7 +35,7 @@
     created() {
       const codigoaluno = this.$route.params.codigoaluno;
       
-      axios.get(`https://apiwebsenac21.azurewebsites.net/alunos/${codigoaluno}`).then(response => {
+      axios.get(`https://apiwebsenac3.azurewebsites.net/alunos/${codigoaluno}`).then(response => {
         this.aluno.nome = response.data[0].nome;
         this.aluno.endereco = response.data[0].endereco;
         this.aluno.telefone = response.data[0].telefone;
@@ -44,7 +44,7 @@
     methods: {
       submitForm() {        
         const codigoaluno = this.$route.params.codigoaluno;
-        axios.put(`https://apiwebsenac21.azurewebsites.net/alunos/${codigoaluno}`, this.aluno).then(() => {
+        axios.put(`https://apiwebsenac3.azurewebsites.net/alunos/${codigoaluno}`, this.aluno).then(() => {
           this.$router.push({ name: 'alunos' });
         });
       }
